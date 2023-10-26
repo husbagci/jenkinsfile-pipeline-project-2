db22ab20bcb859f6b4e3fe085e97f3d8002a0cea
EC2 Default User <ec2-user@ip-172-31-68-205.ec2.internal>
2023-10-26 09:28:20 +0000

    updated java

--- Hello.java
+++ Hello.java
@@ -1,6 +1,6 @@
 public class Hello {
 
     public static void main(String[] args) {
-        System.out.println("Hello from Java and jenkins");
+        System.out.println("Hello from Java andve  jenkins");
     }
 }
